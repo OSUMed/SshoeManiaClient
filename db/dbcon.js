f98d551ed7d885127398cd72b0c7cc6e5602b691
Srikanth Medicherla <mediches@oregonstate.edu>
2021-07-14 23:34:37 -0700

    test3

--- db/dbcon.js
+++ db/dbcon.js
@@ -1,13 +1,31 @@
 
 
 
+
 let mysql = require("mysql");
 let pool = mysql.createPool({
     connectionLimit: 10,
-    host: "localhost",
-    user: "root",
-    password: "123456789",
-    database: "sshoe_mania"
+    host: "us-cdbr-east-04.cleardb.com",
+    user: "b4a7f6d20a82ee",
+    password: "83d718e6",
+    database: "heroku_0982de18366080c"
 });
 
 module.exports.pool = pool;
+
+
+
+
+// let mysql = require("mysql");
+// let pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: "localhost",
+//     user: "root",
+//     password: "123456789",
+//     database: "sshoe_mania"
+// });
+
+// module.exports.pool = pool;
+
+
+
